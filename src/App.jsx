8fe33971eb5bfbcedb0e01384a0ee5f47cbf5524
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-11 19:18:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>RBX Marketplace</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/admin">Admin</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
