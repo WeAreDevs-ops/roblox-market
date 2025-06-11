b0b415c03e09b984193f4c04ad6e9b2f0f2d9c43
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-11 21:10:03 +0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCfx4m9yrc6CaEU83q_AbG_RG4agOvje4",
+  authDomain: "rbx-sm-db.firebaseapp.com",
+  projectId: "rbx-sm-db",
+  storageBucket: "rbx-sm-db.firebasestorage.app",
+  messagingSenderId: "626499721050",
+  appId: "1:626499721050:web:237fd4bf9cb9abd4228563",
+  measurementId: "G-M878D082KB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
