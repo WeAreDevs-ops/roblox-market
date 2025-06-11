889c0e3399312070bdf67711fa0a0065ba6d177d
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-11 21:28:56 +0800

    Update accounts.js

--- api/accounts.js
+++ api/accounts.js
@@ -1,4 +1,4 @@
-import { db } from '../../firebase';
+import { db } from '../firebase';
 import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
 
 export default async function handler(req, res) {
