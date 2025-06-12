42f4b4c6674cfd62603d1c553d748b004518ac35
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-12 08:17:34 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-// Import Vercel Speed Insights
+// Import Vercel Speed Insights & Analytics
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
