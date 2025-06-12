2c4a21f83e3aea482c8ef904ab7e0143b3c87fc2
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-12 21:28:00 +0800

    Update firebaseAdmin.js

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,14 +1,38 @@
-import { getApps, initializeApp, applicationDefault, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import admin from 'firebase-admin';
 
-// Initialize Firebase Admin only once
-if (!getApps().length) {
-  initializeApp({
-    credential: applicationDefault(),  // This will work locally if you have GOOGLE_APPLICATION_CREDENTIALS set
-    projectId: "rbx-sm-db",           // Your exact project ID from firebaseConfig
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: "rbx-sm-db",
+      clientEmail: "firebase-adminsdk-fbsvc@rbx-sm-db.iam.gserviceaccount.com",
+      privateKey: `-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDbr1oXZJisCMhk
+aHn68zIz4G2ti4sbakgctC6kLk1BjzsDYZHEUdU+C2ALGGlDJqrtjGWEN00IPnmx
+IcKcfeBgZOT2uUDFGhgVxgXdctc0YI+ArURkEkMWCO8gnR5c8ZKycjM6zckBjcD7
+lqvyxguK+uQu4yo01tLrtjEJ41zfgHLFvHsnkpqXTvbsvsBjMWOTwpxm2Hes0zCK
+XbMmPtfoZWpXJAq96eENW9sVL5YiMpI1w4H/oYEcCldV0K6L4DaKuiBkmZF6qOFZ
+4Z+UUHearxu4YSO8s9qOA5nL7SW9rhg85FddyY+YXBGEUxGYL2BR5mIwBJOAEZ90
+zgsZ+jfNAgMBAAECggEAAJrkiCD2TK7/gktJXrgRwNPS6YHzYaFyzmVupXQdMVuI
+IZPAt4Y0LKYSXyVzkMRH/zx+QRV36FhntGWv6AduuUU93jIXbBOM0m6HsJry3cmQ
+S53ujMoCFkrzAad9IGtn6Ibo/jX34kG2MNsy4QnLmVfkjf6BLMEUtqJ7UxglttXd
+7KHiFIDYGmof8S1Aum8WBL8mOK/FDIXnJcnmozoWjxzOjH4qpOoCJielce6YC1xS
+CUPfu8bmzoqj3ScywhYDj2TfAYQbcXpzf4pTFJEsoeYqnkcG/8zdiENAVx1ZI840
+S4z0hLicqH9Z1WsV07vHe7cv/LHzZcG19fUJfbg6/wKBgQDuy2y1WO30ahnLaYvm
+rkJ+JPo5rdqXl8ql5vFpc/YTmIVKiI7bCzwfsN3PgRzVJy24CaBuJGTCNEtBINyj
++3HNE8ktka6M6pZhFjauRocQ7fPf/AY/zxZIJURagt0WMSUyfoGZv2ZVpKKersFd
+JziFiQVfeM3oYo/AERtV0+xa8wKBgQDrg3LtriFOldZZLdNARHFyFHvX9rMU2j4x
+uVZot6MholN8R4pa11Zw/y0PuOSxcaMjF7MJAVbdep0unGZWv47TQdpl3WUMPfYr
+Le+O4w3nomEl1PftZDvyq7aXaLTuAFiIfBSjIWTQ2A4TbXC8f7SY14dfEhJe8WMS
+NZthOxpSPwKBgC0kLX+2uPz8VbX7VOQdyxoXmWnv8KO5oShtmPNrZXhc2820NPU5
+kXsYL0Y7zRyKVH027KoFnTqNMjC0qqEhNmkjNWO0539iFV1zGpElagEMSo7sEK5d
+UoP+74YAuRSp47NA8PUElq7X+ZnvxUP71xZ/SZG7v4kPoMcZNMar0kF7AoGAQBd+
+sFYB9GvscEp7QfMeWGhgp44YgCVAZbHFZSwMdfvB8oFZko0llgyri6rKecuuFL+/
+VkUKCa3zlpqzcwogaTdzouMUO7elBW0z01PRFednvq7tZaowckLupte03Uw0y7fQ
+ufA9imQxuHHBC1tJn4qP6Xx2iWXxiyQRi+1UpWsCgYB1CjvEcMj4I9I8TBX/44ew
+nyIuhrqTIhTShMFtAv5d7bQJxkwTc8qKUXSdaRNYRvsLWW0A21CddzMEjEbZx5+6
+Mj3eYy8VeI5eQepFIywHF+vQagN1YzA4QtuCojV9uYvIv/4MM3vQwdp9zYLj1rOk
+iQuVZdz2ziRazeDjhkxovg==\n-----END PRIVATE KEY-----\n`,
+    }),
   });
 }
 
-const db = getFirestore();
-
-export default db;
+export default admin;
