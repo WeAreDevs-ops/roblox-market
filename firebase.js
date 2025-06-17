570b99036d7eddace73b573d3fbb077485c47b15
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-17 21:48:22 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCfx4m9yrc6CaEU83q_AbG_RG4agOvje4",
-  authDomain: "rbx-sm-db.firebaseapp.com",
-  projectId: "rbx-sm-db",
-  storageBucket: "rbx-sm-db.appspot.com",
-  messagingSenderId: "626499721050",
-  appId: "1:626499721050:web:237fd4bf9cb9abd4228563",
-  measurementId: "G-M878D082KB"
+  apiKey: "AIzaSyA51M53loHrRKuHboShHvSQJXKtkgp698E",
+  authDomain: "rbxmarket-6a8b8.firebaseapp.com",
+  projectId: "rbxmarket-6a8b8",
+  storageBucket: "rbxmarket-6a8b8.appspot.com",
+  messagingSenderId: "426561154224",
+  appId: "1:426561154224:web:fef73c5b5f9c3dbc3e5f75"
 };
 
 const app = initializeApp(firebaseConfig);
