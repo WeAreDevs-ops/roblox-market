b22cc47f38852c47828b04da0c475a1761221a42
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-19 19:55:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import Register from './pages/Register';
+import SellerLogin from './pages/SellerLogin';
+import SellerPanel from './pages/SellerPanel';
 
 export default function App() {
-return (
-<BrowserRouter>
-<header>
-<h1>𝗔𝗰𝗰𝗼𝘂𝗻𝘁 𝗠𝗮𝗿𝗸𝗲𝘁𝗽𝗹𝗮𝗰𝗲</h1>
-<nav>
-<Link to="/">DASHBOARD</Link> | <Link to="/admin">LISTING</Link>
-</nav>
-</header>
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/admin" element={<Admin />} />
-</Routes>
-</BrowserRouter>
-);
-}
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>𝗔𝗰𝗰𝗼𝘂𝗻𝘁 𝗠𝗮𝗿𝗸𝗲𝘁𝗽𝗹𝗮𝗰𝗲</h1>
+        <nav>
+          <Link to="/">DASHBOARD</Link> |{" "}
+          <Link to="/admin">LISTING</Link> |{" "}
+          <Link to="/register">REGISTER</Link> |{" "}
+          <Link to="/sellerlogin">SELLER LOGIN</Link> |{" "}
+          <Link to="/sellerpanel">SELLER PANEL</Link>
+        </nav>
+      </header>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/sellerlogin" element={<SellerLogin />} />
+        <Route path="/sellerpanel" element={<SellerPanel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
