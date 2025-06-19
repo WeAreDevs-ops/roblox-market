80cc4373c0db22d488521cf237ef397a8cfefe58
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-20 00:15:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import Register from './pages/Register';
 
 export default function App() {
-return (
-<BrowserRouter>
-<header>
-<h1>𝗔𝗰𝗰𝗼𝘂𝗻𝘁 𝗠𝗮𝗿𝗸𝗲𝘁𝗽𝗹𝗮𝗰𝗲</h1>
-<nav>
-<Link to="/">DASHBOARD</Link> | <Link to="/admin">LISTING</Link>
-</nav>
-</header>
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/admin" element={<Admin />} />
-</Routes>
-</BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>𝗔𝗰𝗰𝗼𝘂𝗻𝘁 𝗠𝗮𝗿𝗸𝗲𝘁𝗽𝗹𝗮𝗰𝗲</h1>
+        <nav>
+          <Link to="/">Dashboard</Link> |{" "}
+          <Link to="/admin">Admin Panel</Link> |{" "}
+          <Link to="/register">Register</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
