8e8e8a4f1dc57ae4bd56d8fc05a2364c29aff47d
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-26 12:43:47 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -137,12 +137,12 @@ async function fetchLimitedItem() {
     resultDiv.innerHTML = `
       <h2>${details.Name}</h2>
       <img src="${thumbnail}" alt="Item Thumbnail" style="max-width: 300px; border-radius: 10px;" />
-      <p><strong>Creator:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${details.Creator?.Name || "N/A"} </span></p>
-      <p><strong>Lowest Resale Price:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${formattedResale}</span></p>
-      <p><strong>BlackMarket 150PHP/1000RBX:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${resaleInPHP}</span></p>
-      <p><strong>Type:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${getAssetTypeName(details.AssetTypeId)}</span></p>
-      <p><strong>Is Limited:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${details.IsLimited ? "✅" : "❌"}</span></p>
-      <p><strong>Is Limited Unique:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#F08080'}; font-weight: bold;"> ${details.IsLimitedUnique ? "✅" : "❌"}</span></p>
+      <p><strong>Creator:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${details.Creator?.Name || "N/A"} </span></p>
+      <p><strong>Lowest Resale Price:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${formattedResale}</span></p>
+      <p><strong>BlackMarket 150PHP/1000RBX:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${resaleInPHP}</span></p>
+      <p><strong>Type:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${getAssetTypeName(details.AssetTypeId)}</span></p>
+      <p><strong>Is Limited:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${details.IsLimited ? "✅" : "❌"}</span></p>
+      <p><strong>Is Limited Unique:</strong><span style="color: ${details.IsLimited ? '#7DC387' : '#7DC387'}; font-weight: bold;"> ${details.IsLimitedUnique ? "✅" : "❌"}</span></p>
     `;
   } catch (error) {
     console.error(error);
@@ -265,7 +265,7 @@ async function fetchLimitedItem() {
           
 ) : viewType === 'limitedChecker' ? (
   <div style={{ textAlign: 'center', marginTop: '30px' }}>
-    <h2 className="text-green-400">🔍 Roblox Limited Item Checker</h2>
+    <h2 style={{ color: "white" }}>Limited Item Checker</h2>
     <input
       id="assetIdInput"
       type="text"
