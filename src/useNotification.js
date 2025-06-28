412e7998a37cfc6468a6309c47c9aa645fde4613
WeAreDevs-ops <cleencuevas@gmail.com>
2025-06-28 13:48:16 +0800

    Create useNotification.js

---
+++ src/useNotification.js
@@ -0,0 +1,23 @@
+// useNotification.js
+export function useNotification() {
+  const requestPermission = async () => {
+    if ('Notification' in window) {
+      try {
+        await Notification.requestPermission();
+      } catch (e) {
+        console.error('Notification permission error:', e);
+      }
+    }
+  };
+
+  const showNotification = (options) => {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      new Notification(options.title, {
+        body: options.body,
+        icon: options.icon
+      });
+    }
+  };
+
+  return { requestPermission, showNotification };
+}
